e13bb7107b19c3ada63c0dfbb9129ae4a02c3f8a
Emma Semilarski <emma.semilarski@gmail.com>
2020-11-29 23:21:32 +0200

    blabla

--- javascript.js
+++ javascript.js
@@ -7,3 +7,14 @@ function otepää(d,s,id) {
     var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
     {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);
 }}
+
+function pärnu(d, s, id) {
+    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+    {js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);
+}}
+
+function türi(d, s, id) {
+    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+    {js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);
+}}
+
