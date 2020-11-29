a4a3b75a8fc53e4076c651425fc9e8f239eaf684
Kertu-Carina Kallaste <kertu-ca@ut.ee>
2020-11-29 21:38:03 +0200

    js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function narva(d,s,id) {
+    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+    {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);
+}}
+
+function otepää(d,s,id) {
+    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+    {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);
+}}
