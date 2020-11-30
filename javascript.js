4b84edd6e5f6546d1f7cccbe72f7bedd8a0cacae
Emma Semilarski <emma.semilarski@gmail.com>
2020-11-30 21:31:25 +0200

    ühine

--- javascript.js
+++ javascript.js
@@ -1,20 +1,4 @@
-function narva(d,s,id) {
-    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
-    {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);
-}}
-
-function otepää(d,s,id) {
-    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
-    {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);
-}}
-
-function pärnu(d, s, id) {
+function ilmaprognoos(d, s, id) {
     var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
     {js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);
 }}
-
-function türi(d, s, id) {
-    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
-    {js=d.createElement(s);js.id=id;js.src="https://weatherwidget.io/js/widget.min.js";fjs.parentNode.insertBefore(js,fjs);
-}}
-
